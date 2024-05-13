eb88f0c5d7f43658be68a2bb73d866fc8eb11ca1
CardKwando <13jthanm@gmail.com>
2024-05-13 17:12:46 -0400

    Modify: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         </a>
       </div>
       
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Bootstrap</h1>
       <div><Message /></div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
