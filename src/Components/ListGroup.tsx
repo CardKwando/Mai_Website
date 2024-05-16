dde8096780b2f81378189218f696b681ea79dc62
CardKwando <13jthanm@gmail.com>
2024-05-15 22:37:50 -0400

    Update: bootstrapLogo, handleClick

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+import { MouseEvent } from 'react';
+
 function ListGroup() {
-    const items = [
+    let items = [
         'Here',
         'There',
         'Nowhere',
     ];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    //event handler
+    const handleClick = (event: MouseEvent) => console.log(event);
 
     return (
         <>
             <h1>List</h1>
+            {items.length === 0 && <p>There are no items</p>}
             <ul className="list group">
-                {items.map((item) => (
-                    <li key={item}> {item} </li>))}
+                {items.map((item, index) => (
+                    <li 
+                        className={
+                            selectedIndex === index 
+                            ? 'list-group-item active' 
+                            : 'list-group-item'} 
+                        key={item} 
+                        onClick={handleClick}
+                    > 
+                        {item} 
+                    </li>))}
             </ul>
         </>
     );
