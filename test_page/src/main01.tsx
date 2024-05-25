a1f04ecda5a7bad2ff395fbb5d5ed08a493da68b
CardKwando <13jthanm@gmail.com>
2024-05-25 15:02:16 -0400

    Modify: renaming test_page files

--- test_page/src/main01.tsx
+++ test_page/src/main01.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
-import App01 from './App.tsx';
+import App01 from './App01.tsx';
 
 ReactDOM.createRoot(document.getElementById('root01')!).render(
   <React.StrictMode>
